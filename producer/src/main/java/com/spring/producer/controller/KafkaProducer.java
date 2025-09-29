0849b9fa67cd2a8cfb22d1b1cf2b6b7f6627279f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-09-29 17:26:08 +0530

    adding sendMessage in producer

---
+++ producer/src/main/java/com/spring/producer/controller/KafkaProducer.java
@@ -0,0 +1,24 @@
+package com.spring.producer.controller;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class KafkaProducer {
+    
+    private final KafkaTemplate<String,String> kafkaTemplate;
+    
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam String message){
+        kafkaTemplate.send("my-topic",message);
+        return "Message sent: " + message;
+    }
+}
