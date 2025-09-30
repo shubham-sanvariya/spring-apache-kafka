93b732a170bee63fc4b449db4edd81d130b5f07d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-09-30 23:50:19 +0530

    feat: update KafkaProducer to send RiderLocation objects instead of String messages

--- producer/src/main/java/com/spring/producer/controller/KafkaProducer.java
+++ producer/src/main/java/com/spring/producer/controller/KafkaProducer.java
@@ -6,19 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.producer.dto.RiderLocation;
+
 @RestController
 @RequestMapping("/api")
 public class KafkaProducer {
     
-    private final KafkaTemplate<String,String> kafkaTemplate;
+    private final KafkaTemplate<String,RiderLocation> kafkaTemplate;
     
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducer(KafkaTemplate<String, RiderLocation> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping("/send")
     public String sendMessage(@RequestParam String message){
-        kafkaTemplate.send("my-topic",message);
-        return "Message sent: " + message;
+        RiderLocation riderLocation = new RiderLocation("rider123",28.61,38.87);
+        kafkaTemplate.send("my-topic", riderLocation);
+        return "Message sent: " + riderLocation.riderId();
     }
 }
