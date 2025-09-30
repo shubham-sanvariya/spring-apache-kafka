50da529e66121d8e23a7a5bbaf006374de26e1ef
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-09-30 17:46:38 +0530

    refactor: update KafkaConsumer methods and change KafkaProducer endpoint to POST

--- producer/src/main/java/com/spring/producer/controller/KafkaProducer.java
+++ producer/src/main/java/com/spring/producer/controller/KafkaProducer.java
@@ -1,7 +1,7 @@
 package com.spring.producer.controller;
 
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,7 @@ public class KafkaProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @GetMapping("/send")
+    @PostMapping("/send")
     public String sendMessage(@RequestParam String message){
         kafkaTemplate.send("my-topic",message);
         return "Message sent: " + message;
